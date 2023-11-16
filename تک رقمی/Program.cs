6e4996eae7407756e94943157b37521a51fd2c15
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-11-16 15:58:09 +0330

    1

---
+++ تک رقمی/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Quera
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumber = Convert.ToInt64(Console.ReadLine());
+            var myList = splitDigits(inputNumber);
+
+            long sum = 0;
+            do
+            {
+                sum = 0;
+                for (int i = 0; i < myList.Count; i++)
+                {
+                    sum += myList[i];
+                }
+                myList.Clear();
+                myList = splitDigits(sum);
+            } while (sum >= 10);
+
+            Console.WriteLine(sum);
+        }
+        static List<long> splitDigits(long n)
+        {
+            var myList = new List<long>();
+            while (n > 0)
+            {
+                myList.Add(n % 10);
+
+                n /= 10;
+            }
+            myList.Reverse();
+            return myList;
+        }
+    }
+}
---
+++ عدد چاپ کن/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Quera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine();
+
+            foreach (var item in n)
+            {
+                Console.Write(item + ": ");
+                var y = (int)item - 48;
+                for (int i = 0; i < y; i++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
