1d801a4b0046b283b300f9ce9dfebfc7c3a9a420
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-16 15:27:32 +0330

    ...

---
+++ جیغ زدن/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Quera
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, s = 1;
+            var result = 2;
+
+            var n = Convert.ToInt32(Console.ReadLine());
+            if (n != 1) result++;
+
+            Console.WriteLine(result);
+        }
+    }
+}
