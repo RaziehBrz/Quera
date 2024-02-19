1d65311823626ad7bec572c33098943e88ab017a
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-19 11:38:01 +0330

    ...

---
+++ خوانا برای انسان/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string result;
+        var list = new List<int>();
+        var t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+        foreach (var m in list)
+        {
+            if (m < 1024) result = $"{m}B";
+            else if (m >= 1024 && m < (1024 * 1024)) result = $"{(m / 1024)}KiB";
+            else result = $"{m / (1024 * 1024)}MiB";
+            Console.WriteLine(result);
+        }
+    }
+}
